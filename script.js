201314ec5ff8431f1f4c672d30f1fa951ffec61f
steve_D <scd346@nyu.edu>
2021-10-17 08:36:42 -0500

    consts set

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const inputValue = document.getElementById('prob-input')
+const inputValueDisplay = document.getElementById('inputValue')
+const submitBtn = document.getElementById('submit-button')
+const returnValue = document.getElementById('returnValue')
 
-
-document.getElementById("returnValue").innerHTML = ()
+let operators = ['+', '-', '*', '/']
