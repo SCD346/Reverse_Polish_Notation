ba3e240386641775d6cff67ab990fec055a0d888
steve_D <scd346@nyu.edu>
2021-10-17 08:40:12 -0500

    comment/explaination of problem

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const submitBtn = document.getElementById('submit-button')
 const returnValue = document.getElementById('returnValue')
 
 let operators = ['+', '-', '*', '/']
+
+// Function RPNotation() takes in an input string.  The string contains operands (numbers) followed by operators.
+// This format is known as Reverse Polish notation (RPN).
+// RPNotation() perfoms the mathematical calculation on a RPN string, such as 10 9 8 7 - + +
+// which has a output of 20.
+
