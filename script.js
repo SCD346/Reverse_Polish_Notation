9faa8f2aa785eded96adf135d74657291edee396
steve_D <scd346@nyu.edu>
2021-10-18 08:56:45 -0500

    clean up comments

--- script.js
+++ script.js
@@ -24,63 +24,38 @@ let operators = ['+', '-', '*', '/']
 
 
 function RPNotation() {
-    //Takes input string, converts it into array (of strings)
+    //STEP 1: Takes input string, converts it into array (of strings)
     let valueAsArray = inputValue.value.split(' ')
 
-    //Convert 'valueAsArray' into an array of numbers only, remove operators(Nans)
+    //STEP 2: Convert 'valueAsArray' into an array of numbers only, remove operators(Nans)
     //10 9 8 7 - + + yields valueAsArray[10, 9, 8, 7]
     var foundNumbers = arraySplitter(valueAsArray)
 
-    //Find the operators in 'valueAsArray' and push them into a new array - 'foundOperators'
+    //STEP 3: Find the operators in 'valueAsArray' and push them into a new array - 'foundOperators'
     //'foundOperators' will now contain the input operators in REVERSE order
     //EXAMPLE- Input: 10 9 8 7 - + + yields -> foundOperators['+', '+', '-']
     var foundOperators = findTheOperators(operators, valueAsArray)
-    console.log(foundOperators)
 
-    // //Combine the operators with the numbers
+    //STEP 4: Combine the operators with the numbers
     var reorderInputArray = combineBack(foundNumbers, foundOperators)
     inputValueDisplay.innerHTML = (inputValue.value)
 
-    // Set final reordered array back into a string
+    //STEP 5: Set final reordered array back into a string
     var finalNumStr = reorderInputArray.join('')
 
-    // Get final result, set finalResult
+    //STEP 6: Get final result, set finalResult
     var finalResult = calculateResult(finalNumStr)
 
-    //Display finalResult in HTML
+    //STEP 7: Display finalResult in HTML
     returnValue.innerHTML = (finalResult)
 
-    //Clear the input field
+    //STEP 8: Clear the input field
     clear()
-
-    return finalResult
 }
 
 
 //HELPER FUNCTIONS
-// 1 Clear input after submit
-function clear(){
-    inputValue.value = ''
-}
-
-// 2 Push operators into its own array.  Reverses the order of the operators.
-function findTheOperators (operators, valueAsArray) {
-    let foundOperators = [];
-    //Loop over each element in 'operators'
-    for (let i = 0; i < operators.length; i++) {
-        //Loop over every element in 'valueAsArray'
-        for (let j = 0; j < valueAsArray.length; j++) {
-            //Check if the elements match
-            if (operators[i] === valueAsArray[j]) {
-                //If there is a match, push it into 'foundOperators'
-              foundOperators.push(operators[i]);
-            }
-        }
-    }
-    return foundOperators;
-}
-
-// 3 Get an array of numbers only from the string array.
+//(STEP 2) Get an array of numbers only from the string array.
 function arraySplitter(valueAsArray) {
     //Convert array of strings into an array of numbers
     let result = valueAsArray.map(i=>Number(i));
@@ -91,8 +66,25 @@ function arraySplitter(valueAsArray) {
     return newArray;
 }
 
-// 4 Combine the operators array (foundOperators) with the numbers array (foundNumbers).
-        //foundOperators are in reverse order from the original
+//(STEP 3) Push operators into its own array.  Reverses the order of the operators.
+function findTheOperators (operators, valueAsArray) {
+    let foundOperators = [];
+    //Loop over each element in 'operators'
+    for (let i = 0; i < operators.length; i++) {
+        //Loop over every element in 'valueAsArray'
+        for (let j = 0; j < valueAsArray.length; j++) {
+            //Check if the elements match
+            if (operators[i] === valueAsArray[j]) {
+                //If there is a match, push it into 'foundOperators'
+                foundOperators.push(operators[i]);
+            }
+        }
+    }
+    return foundOperators;
+}
+
+//(STEP 4) Combine the operators array (foundOperators) with the numbers array (foundNumbers).
+//foundOperators are in reverse order from the original
 function combineBack(foundNumbers, foundOperators) {
     let reorderInputArray = [],
     i, l = Math.min(foundNumbers.length, foundOperators.length);
@@ -104,10 +96,15 @@ function combineBack(foundNumbers, foundOperators) {
     return (reorderInputArray);
 }
 
-// 5 Calculate the final number using Function Constructor. (See NOTE #3)
+//(STEP 6) Calculate the final number using Function Constructor. (See NOTE #3)
 function calculateResult(fn) {
     return new Function('return ' + fn)();
-  }
+}
+
+//(STEP 8) Clear input after submit
+function clear(){
+    inputValue.value = ''
+}
 
 
 //EVENT LISTENERS
@@ -118,11 +115,9 @@ submitBtn.addEventListener('click', RPNotation)
 
 
 //NOTES
+  // #1 - I broke down the problem into small functional steps. I created functions to perform each step. The advantage here is that each step can be more easily replaced/improved by simply targeting the step and associated 'helper function'.
 
-  // #1 - I broke down the problem into small functional steps. I created functions to perform each step.
-  // Advantage here is that each step can be more easily replaced/improved since you can simply target the step and associated 'helper function'.
-
-  // #2 - Almost went with eval() in final step.
+  // #2 - Almost went with eval() in STEP 6.
     // console.log(eval(reorderInputArray.join('')))
     // This appears to be a contraversial solutuion.
     // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval)
